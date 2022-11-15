71453ab13b80f8ae7260aca2ea0c312765ac8aa4
Наталья Ярцева <zabaluna@mail.ru>
2022-11-16 00:12:10 +0300

    Ten for two numbers

---
+++ Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+string ToBin(int num)
+{
+    string result = "";
+    string temp = "";
+    while(num > 0)
+    {
+    temp = Convert.ToString(num%2);
+    result = temp + result;
+    num = num/2; 
+    }
+    return result;
+}
+
+Console.WriteLine(ToBin(2));
+
+
