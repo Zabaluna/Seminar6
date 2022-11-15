196b4fba53aa6926094db0f7c0e92570eefcc7fe
Наталья Ярцева <zabaluna@mail.ru>
2022-11-16 00:22:20 +0300

    newnums=nums array

---
+++ Task45/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива
+//  с помощью поэлементного копирования.
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+int[] nums = CreateRandomArray(10, 0, 51);
+int[] newnums = new int[10];
+
+Console.Write($"Старый массив: [ {String.Join(",", nums)} ]");
+
+for (int i = 0; i < nums.Length; i++)
+{
+    newnums[i] = nums[i];
+}
+Console.WriteLine();
+Console.WriteLine($"Новый массив: [ {String.Join(",", newnums)} ]");
